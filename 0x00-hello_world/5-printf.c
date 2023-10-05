357a168f0056642c21f1e65967ae82f96348d7a2
HebaZaki <h_borhan@hotmail.com>
2023-10-05 12:10:12 -0700

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/** main - printing strings with printf
+ *
+ * printf needs a new line
+ *
+ * Return: 0 on success
+ */
+#include <stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
