eb7f221940248020afbde82fdd74be3cc0d2ca28
HebaZaki <h_borhan@hotmail.com>
2023-10-08 04:24:48 -0700

    determine modulus and if else

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,12 +10,11 @@
 int main(void)
 {
 	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	
 	int a; /* declare a new variable: modulus of n by 10 */
 	a = n % 10;
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	
 	if (a > 5)
 	{
