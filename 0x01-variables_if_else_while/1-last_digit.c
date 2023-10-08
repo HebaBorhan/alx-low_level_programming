51024b0846110e75479b346871ad36c5ef372aa8
HebaZaki <h_borhan@hotmail.com>
2023-10-08 04:04:42 -0700

    determine modulus and if else

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,21 +14,20 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
-	/* declare a new variable: modulus of n by 10 */
-	int a;
+	int a; /* declare a new variable: modulus of n by 10 */
 	a = n % 10;
 	
 	if (a > 5)
 	{
-	printf("Last digit of %d is %d and is greater than 5\n", n, a);
+		printf("Last digit of %d is %d and is greater than 5\n", n, a);
 	}
 	else if (a == 0)
 	{
-	printf("Last digit of %d is %d and is 0\n", n, a);
+		printf("Last digit of %d is %d and is 0\n", n, a);
 	}
 	else if (n < 6)
 	{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 	}
 	return (0);
 }
