a50a6c47b99b9214b8ccfe5a1c513cbd1f117c00
HebaZaki <h_borhan@hotmail.com>
2023-10-08 06:55:27 -0700

    alphabet with putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - printing alphabet using putchar function
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	char alphabet = 'a';
+	for (i = 1; i < 26; i++)
+	{
+		putchar(alphabet);
+
+		return = 0
+	}
