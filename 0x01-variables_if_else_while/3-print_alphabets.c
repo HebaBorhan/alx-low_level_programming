5cf20ef59a0c9929506257e4ffba16a44226c854
HebaZaki <h_borhan@hotmail.com>
2023-10-08 07:30:37 -0700

    alphabet bothe lower and upper with putchar

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - printing alphabet using putchar function
+ * main - printing alphabet both lowercase and uppercase using putchar function
  *
  * Return: 0 for success
  */
 
 int main(void)
 {
-	/* declaring a character variable */
+	/* declaring two character variables */
 	char letterLower = 'a';
 	char letterUpper = 'A';
 
-	/* using for loop for the alphabet from a to z */
+	/* using for loop for the alphabet from a to z and from A to Z */
 	for (letterLower = 'a'; letterLower <= 'z'; letterLower++)
 	{
 		putchar(letterLower);
