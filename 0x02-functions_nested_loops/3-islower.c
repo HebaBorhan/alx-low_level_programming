70e67cfc6498c62555b487c848d7d811a092659e
HebaZaki <h_borhan@hotmail.com>
2023-10-10 12:34:44 -0700

    determine if a letter is lowercase or no

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * int _islower - determine if a letter is lowercase or no
+ * _islower - determine if a letter is lowercase or no
  *
  * Return: 1 if lowercase, 0 otherwise
  */
 
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122) {
+	if (c >= 97 && c <= 122)
+	{
 	/* letter is lowercase */
-	return 1;
-	} 
+	return (1);
+	}
 	else
 	{
 	/* letter is uppercase */
-	return 0;
+	return (0);
 	}
 }
