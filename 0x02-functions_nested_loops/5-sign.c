3305c55022113144a7163791dc95b76f381e4b7a
HebaZaki <h_borhan@hotmail.com>
2023-10-10 13:26:09 -0700

    prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@
 
 int print_sign(int n)
 {
-	if (n>0);
+	if (n > 0)
 	{
 	_putcha('+');
 	return (1);
 	}
-	else if (n>0);
+	else if (n > 0)
 	{
 	_putcha('-');
 	return (-1);
 	}
-	else if (n=0);
+	else if (n == 0)
 	{
 	_putcha(0);
 	return (0);
