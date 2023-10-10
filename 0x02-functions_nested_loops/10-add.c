0dff39545fd28dca6d9d40606bf5820cc979493e
HebaZaki <h_borhan@hotmail.com>
2023-10-10 16:27:30 -0700

    adds two integers and returns the result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,16 +2,15 @@
 
 /**
  * add - adds two integers
- *
+ *@a and @b: integers to be added
  * Return: x result of the add
  */
 
 int add(int a, int b)
 {
 	int x;
-	
+
 	x = a + b;
 
-	_putchar(x + '0');
 	return (x);
 }
