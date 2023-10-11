f14e932a91f8f74bb6c886c7a9d50f3c8d8df614
HebaZaki <h_borhan@hotmail.com>
2023-10-10 17:22:55 -0700

    prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - prinintg every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ * jack_bauer - prinintg every minute of the day
  *
  * Return: 0 for success
  */
@@ -14,13 +14,13 @@ void jack_bauer(void)
 	for (hr = 0; hr <= 23; hr++)
 	{
 		for (mn = 0; mn <= 59; mn++)
-		{
+	{
 	_putchar((hr / 10) + '0');
 	_putchar((hr % 10) + '0');
 	_putchar(':');
 	_putchar((mn / 10) + '0');
 	_putchar((mn % 10) + '0');
 	_putchar('\n');
-		}
+	}
 	}
 }
