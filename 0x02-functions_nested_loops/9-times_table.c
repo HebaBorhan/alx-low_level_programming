283e3756cc5bc28e882603b07be70157a9798639
HebaZaki <h_borhan@hotmail.com>
2023-10-10 18:39:01 -0700

    mult table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,29 +9,30 @@ void times_table(void)
 	int row;
 	int column;
 	int mult;
-	
+
 	for (row = 0; row <= 9; row++)
 	{
 		for (column = 0; column <= 9; column++)
 		{
 			mult = row * column;
-			if (mult < 10)
+
+			if (column > 0)
 			{
+				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar(' ');
-			}
-			
+				
+				if (mult < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+		       }
 			_putchar(mult + '0');
-		       if (column < 9)
-		       {
-			       _putchar(',');
-			       _putchar(' ');
-		       }
-		       }
+		}
 		_putchar('\n');
 	}
 }
