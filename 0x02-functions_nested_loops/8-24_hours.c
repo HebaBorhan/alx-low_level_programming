5d57f26988bf0c0bd714d07ece07144055fdb5e8
HebaZaki <h_borhan@hotmail.com>
2023-10-10 17:28:47 -0700

    prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,17 +10,17 @@ void jack_bauer(void)
 {
 	int hr;
 	int mn;
-
+	
 	for (hr = 0; hr <= 23; hr++)
 	{
 		for (mn = 0; mn <= 59; mn++)
-	{
-	_putchar((hr / 10) + '0');
-	_putchar((hr % 10) + '0');
-	_putchar(':');
-	_putchar((mn / 10) + '0');
-	_putchar((mn % 10) + '0');
-	_putchar('\n');
-	}
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((mn / 10) + '0');
+			_putchar((mn % 10) + '0');
+			_putchar('\n');
+		}
 	}
 }
