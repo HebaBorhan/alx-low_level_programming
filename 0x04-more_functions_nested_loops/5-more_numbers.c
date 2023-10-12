eac5a726e024ce4ed386f0d9dab2d3b1256d5841
HebaZaki <h_borhan@hotmail.com>
2023-10-12 08:40:28 -0700

    prining 10 times the numbers, from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,15 +16,15 @@ void more_numbers(void)
 
 		for (n = 0; n <= 14; n++)
 		{
-			if (n <= 9)
+			if (n > 9)
 			{
-			_putchar(n + '0');
+				_putchar('1');
 			}
 			else
 			{
-				  _putchar('1');
 				  _putchar((n % 10) + '0');
 			}
 		}
+		_putchar('\n');
 	}
 }
