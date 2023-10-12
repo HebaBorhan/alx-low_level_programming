c0cfc4f8af20c976a46ad4408506dfa3370c29fa
HebaZaki <h_borhan@hotmail.com>
2023-10-12 16:34:27 -0700

    print a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,10 +13,10 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (i = 1; i <= size; i++)
+		for (i = 0; i < size; i++)
 		{
 			/* nested loop for identation */
-			for (j = 1; j <= size; j++)
+			for (j = i+1; j < size; j++)
 			{
 				_putchar(' ');
 			}
