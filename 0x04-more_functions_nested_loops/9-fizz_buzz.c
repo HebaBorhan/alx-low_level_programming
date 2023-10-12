13c5be32343470bb72306ba4fd9478c165278073
HebaZaki <h_borhan@hotmail.com>
2023-10-12 09:51:30 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main()
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if (((i % 3) == 0) && ((i % 5) == 0))
 		{
@@ -27,5 +27,9 @@ int main()
 		else
 			printf("%d ", i);
 	}
+
+	if (i == 100)
+		printf("Buzz");
+	
 	return (0);
 }
