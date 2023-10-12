b83aa02711ee58ec04e0f48d4d2b1c8f1466cd54
HebaZaki <h_borhan@hotmail.com>
2023-10-12 06:13:10 -0700

    prining numbers except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#inclde "main.h"
+
+/**
+ * print_most_numbers - prints the numbers except 2 and 4
+ *
+ */
+
+void print_most_numbers(void)
+{
+	/* declaring an integer variable */
+	int i = 0;
+
+	/* for loop for numbers from 0 to 9 */
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			/* converting the integer into numeral ASCII character */
+			_putchar(i + '0');
+		}
+	}
+}
