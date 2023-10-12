132a118c27a427a96181111e58d200dd62d23c5f
HebaZaki <h_borhan@hotmail.com>
2023-10-12 05:33:51 -0700

    prining numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_numbers - printing numbers from 0 to 9
+ *
+ * Return: 0 for success
+ */
+
+void print_numbers(void)
+{
+	/* declaring an integer variable */
+	int i = 0;
+
+	/* using for loop for numbers from 0 to 9 */
+	for (i = 0; i <= 9; i++)
+	{
+		/* converting the integer into numeral ASCII character */
+		_putchar(i + '0');
+	}
+	_putchar ('\n');
+	return (0);
+}
