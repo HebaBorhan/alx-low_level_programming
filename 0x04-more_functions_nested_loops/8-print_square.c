e466178a61906babd1b3e217b5736906d09453f1
HebaZaki <h_borhan@hotmail.com>
2023-10-12 13:56:44 -0700

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ *@size: number of times the character # should be printed
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+
+		for (i = 1; i <= size; i++)
+		{
+			/* nested loop for repeating squares */
+			for (j = 1; j < i; j++)
+			{
+				_putchar('#')
+			}
+		}
+		_putchar('#');
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
