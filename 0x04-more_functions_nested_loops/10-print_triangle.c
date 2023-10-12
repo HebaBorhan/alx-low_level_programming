164d181659f760d3cd4f5e1303edf640091b1dfe
HebaZaki <h_borhan@hotmail.com>
2023-10-12 15:03:34 -0700

    print a triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle - draws a triangle
+ *@size: number of times the characters # should be printed
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			/* nested loop for identation */
+			for (j = 1; j < i; j++)
+			{
+				_putchar(' ');
+			}
+
+			/* nested loop for repeating squares */
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+		}
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
