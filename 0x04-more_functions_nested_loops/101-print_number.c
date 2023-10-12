46b14088eb76eb192d4f05eaab061a1aeb6c6a06
HebaZaki <h_borhan@hotmail.com>
2023-10-12 15:38:28 -0700

    print an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,26 +1,21 @@
 #include "main.h"
 
 /**
- *  print_last_digit - prints the last digit of a number
- *@n: The integer to be checked
- *
- * Return: a on success which is last digit value
+ * print_number - prints an integer
+ *@n: The integer to be printed
  */
 
-int print_last_digit(int n)
+void print_number(int n)
 {
-	/* variable for last digit */
-	int a;
-
 	if (n < 0)
 	{
-	a = -1 * (n % 10);
-	}
-	else
-	{
-	a = n % 10;
+		n = -1 * n
 	}
 
-	_putchar(a + '0');
-	return (a);
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar((n % 10) + '0');
 }
