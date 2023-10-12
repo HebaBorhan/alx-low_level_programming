406b1746f52ef3b3a35fa8683921b22b1dfdf589
HebaZaki <h_borhan@hotmail.com>
2023-10-12 15:19:43 -0700

    header file

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *  print_last_digit - prints the last digit of a number
+ *@n: The integer to be checked
+ *
+ * Return: a on success which is last digit value
+ */
+
+int print_last_digit(int n)
+{
+	/* variable for last digit */
+	int a;
+
+	if (n < 0)
+	{
+	a = -1 * (n % 10);
+	}
+	else
+	{
+	a = n % 10;
+	}
+
+	_putchar(a + '0');
+	return (a);
+}
