38e6b202cdd12a76893e4dda98c03e6c6e673406
HebaZaki <h_borhan@hotmail.com>
2023-10-16 19:13:39 -0700

    prints half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,4 +2,22 @@
 
 /**
  * puts_half -  prints half of a string
- *
+ *@str: strings to be printed
+ */
+
+puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+
+	i++;
+	
+	for (i = (i / 2); stri[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
