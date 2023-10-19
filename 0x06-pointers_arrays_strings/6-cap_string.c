4b1f3ae825711f15bfae0d9a9e4372b808a8ea4d
HebaZaki <h_borhan@hotmail.com>
2023-10-20 00:52:02 +0300

    changes all lowercase letters of a string to uppercase

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,13 +11,18 @@ char *cap_string(char *str)
 {
 	int i;
 
+	if (str[0] >= 97 && str[0] <= 122)
+	{
+		str[0] = str[0] -32;
+	}
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if ((i == 0 || str[i] == ' ' || str[i] == '\t'
+		if (str[i] == ' ' || str[i] == '\t'
 || str[i] == '\n' || str[i] == ','
 || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?'
 || str[i] == '\"' || str[i] == '(' || str[i] == ')' || str[i] == '{'
-|| str[i] == '}'))
+|| str[i] == '}')
 		{
 			if (str[i + 1] >= 97 && str[i + 1] <= 122)
 			{
