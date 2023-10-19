1192a8b5a4ead9900588afccb13549e02f28cbca
HebaZaki <h_borhan@hotmail.com>
2023-10-19 14:01:21 +0300

    concatenates two strings copying n bytes from src

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,7 +18,7 @@ char *_strncat(char *dest, char *src, int n)
 	{
 	}
 
-	for (j = 0; src[j] <= n && src[j] != '\0'; j++)
+	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
 		dest[i + j] = src[j];
 	}
