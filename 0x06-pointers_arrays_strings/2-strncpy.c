493ac629ce51fe1dfdcf3cfc09f9018c2888f43a
HebaZaki <h_borhan@hotmail.com>
2023-10-19 16:58:19 +0300

    copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copies a string
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
