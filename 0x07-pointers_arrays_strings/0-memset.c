908ffda7e9969581fec9493a792c387e5b3da9fe
HebaZaki <h_borhan@hotmail.com>
2023-10-23 11:38:57 +0300

    fills memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *_memset - fills memory with a constant byte
+ * *_memset - fills memory with a constant byte.
  * @s: pinter to the memory
  * @b: constant byte
  * @n: number of bytes to be filled
@@ -11,12 +11,13 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
 
 	return (s);
 }
+
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: string to be checked
+ * @c: character to look for
+ *
+ * Return: pointer to first occurrence of character c in string s
+ * NULL if the character is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+
+	return (NULL);
+}
