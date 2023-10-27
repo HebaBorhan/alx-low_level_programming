f6c9a04a2a5d449e0db204955eb74af919fea1c3
HebaZaki <h_borhan@hotmail.com>
2023-10-27 23:17:08 +0200

    prints the number of arguments passed into a prog

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,17 @@
-#include <stdio.h>#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed into prog
+ *@argc: integer
+ *@argv: array of string
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
