da412ed661f1d6d5cfde17575f7787504b0557be
HebaZaki <h_borhan@hotmail.com>
2023-10-28 02:31:26 +0200

    adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,1 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ *@argc: integer
+ *@argv: array of string
+ *
+ * Return: integer result of the multiplication
+ * 1 on error
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) != 3)
+	{
+		mult = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mult);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
