c63139a061572441cd2e85e10a64c5e3482bd1c3
HebaZaki <h_borhan@hotmail.com>
2023-11-03 00:32:26 +0200

    allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
 /**
  * *malloc_checked - allocates memory using malloc
@@ -11,7 +14,7 @@ void *malloc_checked(unsigned int b)
 {
 	unsigned int *ptr;
 
-	*ptr = malloc(b);
+	ptr = malloc(b);
 
 	if (ptr == 0)
 	{
