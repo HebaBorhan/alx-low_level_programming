7c3a3085452868eb9dec1d5f767c8dca8b0665b5
HebaZaki <h_borhan@hotmail.com>
2023-11-12 21:37:28 +0200

    get op function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,12 +1,36 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * get_op_func - return pointer to function
+ * @s: string operator
+ *
+ * Return: pointer
+ */
+
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		        {"+", op_add},
-			        {"-", op_sub},
-				        {"*", op_mul},
-					        {"/", op_div},
-						        {"%", op_mod},
-							        {NULL, NULL}
-			    };
-	    int i;
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i;
+
+	i = 0;
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return ('\0');
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,2 +1,47 @@
 #include "calc.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argv: argument value
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char **argv)
+{
+	int (*f)(int a, int b);
+	int calc;
+	int num1, num2;
+	char *s;
+
+	if (argc > 4)
+	{
+		printf('\n');
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	s = argv[2];
+
+	if (num1 == NULL || num2 == NULL)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	f = get_op_func(s);
+
+	if (!(f))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	calc = f(num1, num2);
+	printf("%d\n", calc);
+	return (0);
+}
