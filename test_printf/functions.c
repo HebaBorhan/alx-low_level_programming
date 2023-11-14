f90ec3d27506b4a5a93190b5ff0bb5b9783e1ac5
HebaZaki <h_borhan@hotmail.com>
2023-11-14 18:47:07 +0200

    test

---
+++ test_printf/functions.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stddef.h>
+#include <unistd.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @string: the string to be checked
+ *
+ * Return: i which is the length of the string
+ */
+
+int _strlen(char *string)
+{
+	int length;
+
+	for (length = 0; string[length] != '\0'; length++)
+	{
+	}
+
+	return (length);
+}
+
+int print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	write(1, &c, 1);
+	return (1);
+}
+
+int print_string(va_list args)
+{
+	char *string = va_arg(args, char *);
+
+	if (string == NULL)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+	
+		int length = _strlen(string);
+		write(1, string, length);
+		return (length);
+}
+
+int print_per(va_list args)
+{
+	char b = '%';
+	write(1, &b, 1);
+	return (1);
+}
--- test_printf/main.h
+++ test_printf/main.h
@@ -1,6 +1,26 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+
+
+/**
+ * struct spec - Struct of specifiers
+ * @spec: specifiers
+ * @f: function associated
+ */
+
+typedef struct spec
+{
+    char *spec;
+
+    int (*f)(va_list);
+}spec_t;
+
+int _strlen(char *string);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_per(va_list args);
 int _printf(const char *format, ...);
 
 #endif
