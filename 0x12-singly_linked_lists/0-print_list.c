bdbabc7fa53795050adc56bb7e6f103af0e48c0d
HebaZaki <h_borhan@hotmail.com>
2023-11-17 00:47:30 +0200

    printlist

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,11 +18,11 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nil)\n");
+			printf("[%ld] (nil)\n", count);
 		}
 		else
 		{
-		printf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		h = h->next;
 		count++;
