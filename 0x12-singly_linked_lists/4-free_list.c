a32589e112dd1820598ff419152d7b727ecf5f81
HebaZaki <h_borhan@hotmail.com>
2023-11-17 04:44:15 +0200

    frees a list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,5 +10,13 @@
 
 void free_list(list_t *head)
 {
-	free(head);
-}
+	list_t *current_node = head;
+	list_t *next_node;
+
+	while (current_node != NULL)
+	{
+		next_node = current_node->next;
+		free(current_node);
+		free(current_node);
+		current_node = next_node;
+	}
