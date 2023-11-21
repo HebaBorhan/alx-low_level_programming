ef0919fc806ccb7ceb4403d22052fb72a944f828
HebaZaki <h_borhan@hotmail.com>
2023-11-21 20:43:05 +0200

    frees a list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -16,7 +16,6 @@ void free_listint(listint_t *head)
 	while (current_node != NULL)
 	{
 		next_node = current_node->next;
-		free(current_node->str);
 		free(current_node);
 		current_node = next_node;
 	}
