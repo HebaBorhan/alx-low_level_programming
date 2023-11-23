b0333148c26b34375bd62c0d8821276c0fa19dd7
HebaZaki <h_borhan@hotmail.com>
2023-11-23 23:20:24 +0200

    value of bit at index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * get_bit - value of a bit at a given index
+ * @n: int
+ * @index: index to find value at
+ *
+ * Return: int value of bit
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	else
+	{
+		if ((n >> index & 1) == 0)
+		{
+			return(0);
+		}
+		else
+		{
+			return (1);
+		}
+	}
+}
