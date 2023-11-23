471740cdcfc11528b45ced34dda291395aeccce6
HebaZaki <h_borhan@hotmail.com>
2023-11-24 01:31:42 +0200

    bits to flip from n to m

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,6 +13,11 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int count = 0;
 	unsigned int result = (n ^ m);
 
+	if (result == 0)
+	{
+		count = 0;
+	}
+
 	while (result != 0)
 	{
 		if ((result & 1) == 1)
