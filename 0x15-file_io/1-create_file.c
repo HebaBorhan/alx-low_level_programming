683da6a6f3ca33538fad991fbd3176249ae74127
HebaZaki <h_borhan@hotmail.com>
2023-11-27 23:40:15 +0200

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -37,14 +37,21 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+	}
+	else
+	{
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	}
 
 	if (fd == -1)
 	{
 		return (-1);
 	}
 
-	if (text_content != NULL && length != 0)
+	if (length != 0)
 	{
 		count = write(fd, text_content, length);
 	}
