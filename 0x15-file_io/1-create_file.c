6049ff2400830f1ec23a17c1a469b96e699a32e8
HebaZaki <h_borhan@hotmail.com>
2023-11-27 23:36:06 +0200

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -44,7 +44,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (length != 0)
+	if (text_content != NULL && length != 0)
 	{
 		count = write(fd, text_content, length);
 	}
