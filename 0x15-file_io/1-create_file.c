9d475bd6701ec412ab090359a266401d6ed5c108
HebaZaki <h_borhan@hotmail.com>
2023-11-27 23:00:30 +0200

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+ *_strlen - returns the length of a string
+ * @s: the string to be checked
+ *
+ * Return: int which is the length of string
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+
+/**
+ * create_file - creates a file
+ * @filename: file name
+ * @text_content: NULL terminated string
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int count = 0;
+	int length = _strlen(text_content);
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (length != '\0')
+	{
+		count = write(fd, text_content, length);
+
+		if (count == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+
+	return (1);
+}
