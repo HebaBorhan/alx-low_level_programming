2400854f71ce199c79f812c58b00c43e69638955
HebaZaki <h_borhan@hotmail.com>
2023-11-27 20:00:03 +0200

    reads text file and prints it to standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads text file and prints it to standard output
+ * @filename: file name
+ * @letters: number of letters to read and print
+ *
+ * Return: int numb of letters read & printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	ssize_t count = 0;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	count = write(STDOUT_FILENO, &buffer, count);
+
+	free (buffer);
+	close(fd);
+	return (count);
+}
