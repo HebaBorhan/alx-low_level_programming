71dd042ad1f04bf49d7eb66dbacca5aacb92a3a6
HebaZaki <h_borhan@hotmail.com>
2024-01-11 21:25:49 +0200

    delete at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -41,7 +41,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		{
 			return (-1);
 		}
-		
+
 		temp->prev->next = temp->next;
 		temp->next->prev = temp->prev;
 		free(temp);
