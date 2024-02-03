c5be2d1c02fec32a244c1233347a4c36a1e23d42
HebaZaki <h_borhan@hotmail.com>
2024-02-03 13:46:47 +0200

    add element

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -64,6 +64,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
         tmp = tmp->next;
     }
     new_node = add_node(key, value);
+    if (new_node == NULL)
+        return (0);
     new_node->next = ht->array[index];
     ht->array[index] = new_node;
     return (1);
