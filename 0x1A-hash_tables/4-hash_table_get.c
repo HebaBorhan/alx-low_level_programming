9ac88874ef084934e1382d2ece519c7f476df011
HebaZaki <h_borhan@hotmail.com>
2024-02-04 12:56:43 +0200

    search value

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: pointer to table
+ * @key: pointer to the key
+ *
+ * Return: value associated with key, or NULL if key not found
+*/
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+unsigned long int index = key_index((const unsigned char *)key, ht->size);
+hash_node_t *item = ht->array[index];
+
+if (item != NULL)
+{
+if (strcmp(item->key, key) == 0)
+{
+return (item->value);
+}
+}
+
+return (NULL);
+}
