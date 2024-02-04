ac40644591be07594b82ab7e3cfd1abbe4333b33
HebaZaki <h_borhan@hotmail.com>
2024-02-04 16:04:28 +0200

    print ht

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -12,6 +12,7 @@
 void hash_table_print(const hash_table_t *ht)
 {
 unsigned long int i;
+hash_node_t *node;
 
 if (ht == NULL || ht->size == 0 || ht->array == NULL)
 return;
@@ -20,9 +21,13 @@ i = 0;
 printf("{");
 while (i < ht->size)
 {
-if (ht->array[i] != NULL)
+node = ht->array[i];
+while (node != NULL)
 {
-printf("'%s': '%s', ", ht->array[i]->key, ht->array[i]->value);
+printf("'%s': '%s'", node->key, node->value);
+if (node->next != NULL && i < ht->size - 1)
+printf(", ");
+node = node->next;
 }
 i++;
 }
