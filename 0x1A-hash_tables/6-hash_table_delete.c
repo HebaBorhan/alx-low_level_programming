7d04a7fca1773ece6b895d7c09145eaeb2e3472b
HebaZaki <h_borhan@hotmail.com>
2024-02-07 12:56:33 +0200

    deletes hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * hash_table_print - prints hash table
+ * @ht: pointer to table
+ *
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+unsigned long int i, items_printed;
+hash_node_t *node;
+
+if (ht == NULL || ht->size == 0 || ht->array == NULL)
+return;
+
+i = 0, items_printed = 0;
+printf("{");
+while (i < ht->size)
+{
+node = ht->array[i];
+while (node != NULL)
+{
+if (items_printed > 0)
+{
+printf(", ");
+}
+printf("'%s': '%s'", node->key, node->value);
+items_printed++;
+node = node->next;
+}
+i++;
+}
+printf("}");
+printf("\n");
+}
