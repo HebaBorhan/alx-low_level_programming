46a52e9f63a02e8e55bf1b81f552e5a0ba0a1b25
HebaZaki <h_borhan@hotmail.com>
2024-03-28 21:56:40 +0200

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,18 +4,17 @@
 
 def island_perimeter(grid):
     """returns the perimeter of an island"""
-     perimeter = 0
+    perimeter = 0
     rows = len(grid)
     cols = len(grid[0])
 
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                perimeter += 4  # Each land cell contributes 4 to the perimeter
-                # Check neighboring cells and reduce perimeter for each adjacent land cell
+                perimeter += 4
                 if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2  # Subtract 2 if top neighbor is land
+                    perimeter -= 2
                 if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2  # Subtract 2 if left neighbor is land
+                    perimeter -= 2
 
     return perimeter
