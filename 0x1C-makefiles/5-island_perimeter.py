1fca302804024fef54aceead01cb98c2c8c26bb2
HebaZaki <h_borhan@hotmail.com>
2024-03-28 21:55:50 +0200

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,19 +4,18 @@
 
 def island_perimeter(grid):
     """returns the perimeter of an island"""
-    if not grid:
-        return 0
+     perimeter = 0
     rows = len(grid)
     cols = len(grid[0])
-    visited = [[False] * cols for  _ in range(rows)]
-    def dfs(row, col):
-        """depth first search helper function"""
-        if row < 0 or row >=  rows or col < 0 or col >= cols or visited[row][col]:
-            return
-        visited[row][col] = True
-        directions = [(1, 0),  (-1, 0), (0, 1), (0, -1)]
-        for direction in directions:
-            next_row, next_col =  row + direction[0], col + direction[1]
-            dfs(next_row, next_col)
 
-    count = 0
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4  # Each land cell contributes 4 to the perimeter
+                # Check neighboring cells and reduce perimeter for each adjacent land cell
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2  # Subtract 2 if top neighbor is land
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2  # Subtract 2 if left neighbor is land
+
+    return perimeter
